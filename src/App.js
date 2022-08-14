dbab2376df21098b7aba8c0ad7c5240c711f11d9
leo carvalho <lndrcrvlh@gmail.com>
2022-08-14 16:40:34 -0400

    Adds styles folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 import Banner from './components/Banner';
 import Navbar from './components/Navbar';
 import Contatos from './components/Contatos';
