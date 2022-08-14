97e2db3680dd677803d9d5caee2524d040175694
leo carvalho <lndrcrvlh@gmail.com>
2022-08-14 16:38:53 -0400

    Starts assets folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner';
+import Navbar from './components/Navbar';
+import Contatos from './components/Contatos';
+import Blog from './components/Blog';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar></Navbar>
+    <Banner></Banner>
+    <Blog></Blog>
+    <Contatos></Contatos>
+    </>
+    
   );
 }
 
